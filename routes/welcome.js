4929e62d587a6edf47c73ca0b1243ff9bcf103e2
Rmukati1 <rishmuka@iitk.ac.in>
2022-03-13 19:04:27 +0530

    feedbackbackend

--- routes/welcome.js
+++ routes/welcome.js
@@ -8,6 +8,7 @@ const validateDate = require("validate-date");
 // Load User model
 const User = require('../models/User');
 const Travel = require('../models/travel');
+const Feedback = require('../models/feedback');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
@@ -22,6 +23,38 @@ router.get("/feedback", function (req, res) {
 
 router.get("/contact", function (req, res) {
   res.render("contact");
+});
+
+router.post("/feedback", function(req , res ) {
+  const { star1, star2, star3, star4, star5, comment, time  } = req.body;
+  let errors = [];
+  if (errors.length > 0) {
+    res.render('travelform', {
+      errors,
+      star1,
+      star2,
+      star3,
+      star4,
+      star5,
+      comment,
+      time 
+    });
+  } else {
+
+        const newFeedback = new Feedback({
+          errors,
+          star1,
+          star2,
+          star3,
+          star4,
+          star5,
+          comment,
+          time 
+        }); 
+        console.log(newFeedback)
+        newFeedback.save()
+        res.redirect('/feedback');
+      }    
 });
 
 //About
